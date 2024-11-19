3f6d70294f3d284404c471a93e3d0992b3eb4992
Vita Koval <vita@expiria.co>
2024-11-19 12:38:01 -0700

    add base component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Flex } from '@/ui'
+
+function App() {
+  return (
+    <>
+      <Flex height="150px" bg="pink">
+        <h1>Hello!</h1>
+      </Flex>
+    </>
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,30 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  plugins: [react()],
+  plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        })
+      },
+    },
+    react(),
+  ],
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
