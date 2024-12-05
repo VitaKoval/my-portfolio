26343955fb517234ffb60de843c14db66311f422
Vita Koval <vita@expiria.co>
2024-12-05 13:22:49 -0700

    fix definitions project

--- src/components/Projects/definitions.js
+++ src/components/Projects/definitions.js
@@ -1,5 +1,5 @@
-import SoliticsImgIntro from '../../assets/images/Projects/solitics-intro.png'
-import PuzzleImgIntro from '../../assets/images/Projects/puzzle-intro-1.png'
+// import SoliticsImgIntro from '../../assets/images/Projects/solitics-intro.png'
+// import PuzzleImgIntro from '../../assets/images/Projects/puzzle-intro-1.png'
 
 // props from ProjectItem ===>>> [{ path, title, subtitle, image, link, technologies }]
 
@@ -8,7 +8,7 @@ export const projectsList = [
     path: '/my-portfolio/solitics-website',
     title: 'Solitics website',
     subtitle: '',
-    image: SoliticsImgIntro,
+    image: '',
     link: 'https://solitics.com/',
     technologies: [
       'HTML',
@@ -24,7 +24,7 @@ export const projectsList = [
     path: '/my-portfolio/pazzle-app',
     title: 'Puzzle (event app)',
     subtitle: '',
-    image: PuzzleImgIntro,
+    image: '',
     link: 'https://wogi.io/puzzle-room/register/10',
     technologies: [
       'React',
